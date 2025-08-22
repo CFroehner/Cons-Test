74afbc6f7fd637d5b0053e18900b5b1d441e0203
Cosima Fröhner <96821405+CFroehner@users.noreply.github.com>
2025-08-22 11:54:13 +0100

    Stylized landscape folder commit
    removed povmap-grdi-v1.tif because of size issues

---
+++ StylizedLandscape/DeprivationDistribution.R
@@ -0,0 +1,8 @@
+library(raster)
+library(sf)
+Dep <- raster::raster("StylizedLandscape/povmap-grdi-v1.tif")
+AOI<-read_sf("StylizedLandscape/AOI.shp")
+Dep<-raster::crop(Dep,AOI)
+Dep<-raster::as.data.frame(Dep,xy=T)
+Dep<-Dep[complete.cases(Dep), ] 
+names(Dep)[3]<-"Deprivaton"
